450207423b879ba78f6d05bdeddead515325e620
Christian Eufrasio da Silva <christian.eds@hotmail.com>
2019-01-24 18:57:51 -0200

    initial commit

---
+++ NgTodoList.Domain.Tests/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NgTodoList.Domain.Tests
+{
+    [TestClass]
+    public class Dado_um_novo_usuario
+    {
+        [TestMethod]
+        [TestCategory("User - Novo Usuário")]
+        [ExpectedException(typeof(Exception))]
+        public void O_nome_deve_ser_valido()
+        {
+            var user = new User("", "christian.eds@hotmail.com", "123");
+        }
+
+        [TestMethod]
+        [TestCategory("User - Novo Usuário")]
+        [ExpectedException(typeof(Exception))]
+        public void O_email_deve_ser_valido()
+        {
+
+        }
+    }
+}
---
+++ NgTodoList.Domain/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NgTodoList.Domain
+{
+    public class User
+    {
+        private IList<Todo> _todos;
+
+        protected User() { }
+
+        public User(string name, string email, string password)
+        {
+            if (name.Length < 3)
+                throw new Exception("Nome inválido");
+        }
+
+        public int Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public string Password { get; protected set; }
+        public bool IsActive { get; protected set; }
+        public virtual ICollection<Todo> Todos
+        {
+            get { return _todos; }
+            protected set { _todos = new List<Todo>(value); }
+        }
+    }
+}
