a4841ff442ce01af9276ccbbbedd928baa6cd616
Christian Eufrasio da Silva <christian.eds@hotmail.com>
2019-01-25 00:10:04 -0200

    add data

---
+++ NgTodoList.Data/Context/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using NgTodoList.Data.Mappings;
+using NgTodoList.Domain;
+using System.Data.Entity;
+
+namespace NgTodoList.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            :base("NgTodoListConnectionString")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserMap());
+            modelBuilder.Configurations.Add(new TodoMap());
+        }
+    }
+
+    public class DataContextInitializer : DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ NgTodoList.Data/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using NgTodoList.Data.Context;
+using NgTodoList.Domain;
+using NgTodoList.Domain.Repositories;
+using System.Data.Entity;
+using System.Linq;
+
+namespace NgTodoList.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            _context.Users.Remove(_context.Users.Find(id));
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public User Get(string email)
+        {
+            return _context.Users.Where(x => x.Email.ToLower() == email.ToLower()).FirstOrDefault();
+        }
+
+        public void SaveOrUpdate(User user)
+        {
+            if (user.Id == 0)
+                _context.Users.Add(user);
+            else
+                _context.Entry(user).State = EntityState.Modified;
+
+            _context.SaveChanges();
+        }
+    }
+}
